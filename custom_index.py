b47ec1e10442ed819641e1ce8d007cadef807bbc
Liam-SC <sclee@alphacode.co.kr>
2023-06-08 15:14:25 +0900

    [fix] llm context edit (openai to customllm)

--- custom_index.py
+++ custom_index.py
@@ -16,6 +16,7 @@ from llama_index import (
     Document,
     SimpleDirectoryReader,
     download_loader,
+    set_global_service_context
 )
 from llama_index.llm_predictor import HuggingFaceLLMPredictor
 from pathlib import Path
@@ -67,7 +68,8 @@ for file in os.listdir(args.path):
         if documents is None:
             documents = d
         else:
-            documents.append(d)
+            for innerd in d:
+                documents.append(innerd)
 
 
 
@@ -79,7 +81,6 @@ query_str = QA_PROMPT_TMPL = (
     "Given this information, I want you to act as insurance assistance. I’ll write you my insurance info and my diagnosis , and you’ll inform for me which guaranteed amount, info from via my insurance terms and answer this : {query}\n"
 )
 
-
 hf_predictor = HuggingFaceLLMPredictor(
     max_input_size=2048,
     max_new_tokens=256,
@@ -87,11 +88,12 @@ hf_predictor = HuggingFaceLLMPredictor(
     tokenizer_name=args.model,
     model_name=args.model,
     tokenizer_kwargs={"max_length": 2048},
-    model_kwargs={"offload_folder": "offload", "torch_dtype": torch.bfloat16},
+    model_kwargs={"offload_folder": args.model + "_offload", "torch_dtype": torch.bfloat16},
 )
 
+
 embed_model = LangchainEmbedding(
-    HuggingFaceEmbeddings(model0_name="jhgan/ko-sroberta-multitask")
+    HuggingFaceEmbeddings(model_name="jhgan/ko-sroberta-multitask")
 )
 
 
@@ -99,13 +101,15 @@ service_context = ServiceContext.from_defaults(
     chunk_size_limit=512, llm_predictor=hf_predictor, embed_model=embed_model
 )
 
+set_global_service_context(service_context)
+
 
 QA_PROMPT = QuestionAnswerPrompt(QA_PROMPT_TMPL)
 index = VectorStoreIndex.from_documents(documents)
 
 index.storage_context.persist()
 
-engine = index.as_query_engine(text_qa_template=QA_PROMPT)
+engine = index.as_query_engine(streaming=True, text_qa_template=QA_PROMPT)
 response = engine.query(query_str)
 
 print(response)
