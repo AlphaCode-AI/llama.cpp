8d253096e1defbe34069bd2ca3bc7ad28076bdfc
Liam-SC <sclee@alphacode.co.kr>
2023-06-08 13:22:19 +0900

    [feat] Add IndexLoader with params

--- custom_index.py
+++ custom_index.py
@@ -1,27 +1,29 @@
-import torch
 from langchain.llms.base import LLM
-from llama_index import SimpleDirectoryReader, LangchainEmbedding, ListIndex
-from llama_index import LLMPredictor, ServiceContext
+
+import argparse
+import os
+import torch
+
 from transformers import pipeline
+from llama_index import (
+    GPTListIndex,
+    QuestionAnswerPrompt,
+    PromptHelper,
+    ServiceContext,
+    VectorStoreIndex,
+    Document,
+    SimpleDirectoryReader,
+    download_loader,
+)
+from llama_index.llm_predictor import HuggingFaceLLMPredictor
+from pathlib import Path
 from typing import Optional, List, Mapping, Any
 
 
-# set context window size
-context_window = 2048
-# set number of output tokens
-num_output = 256
-
-# store the pipeline/model outisde of the LLM class to avoid memory issues
-model_name = "beomi/KoAlpaca"
-pipeline = pipeline("qa", model=model_name, device="cuda:0", model_kwargs={"torch_dtype":torch.bfloat16})
-
 class CustomLLM(LLM):
-    
     def _call(self, prompt: str, stop: Optional[List[str]] = None) -> str:
         prompt_length = len(prompt)
         response = pipeline(prompt, max_new_tokens=num_output)[0]["generated_text"]
-
-        # only return newly generated tokens
         return response[prompt_length:]
 
     @property
@@ -32,20 +34,94 @@ class CustomLLM(LLM):
     def _llm_type(self) -> str:
         return "custom"
 
-# define our LLM
-llm_predictor = LLMPredictor(llm=CustomLLM())
 
-service_context = ServiceContext.from_defaults(
-    llm_predictor=llm_predictor, 
-    context_window=context_window, 
-    num_output=num_output
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-m", 
+    "--model", 
+    dest="model", 
+    default="beomi/KoAlpaca", 
+    action="store"
+)
+parser.add_argument(
+    "-p", 
+    "--path", 
+    dest="path", 
+    default="./", 
+    action="store")
+parser.add_argument(
+    "-d", 
+    "--device", 
+    dest="device", 
+    default="cpu", 
+    action="store")
+parser.add_argument(
+    "-q",
+    "--query",
+    dest="query",
+    default="저는 가입 기간은 1년 미만이며 주계약은 가입금액 1000만원, 암진단특약VID는 가입금액 500만원으로 가입하였습니다. 유방암 진단을 받은 경우 지급금액이 얼마인가요?",
+    action="store",
+)
+args = parser.parse_args()
+
+context_window = 2048
+num_output = 256
+
+pipeline = pipeline(
+    model=args.model, device=args.device, model_kwargs={"torch_dtype": torch.bfloat16}
+)
+CJKPDFReader = download_loader("CJKPDFReader")
+loader = CJKPDFReader()
+
+documents = None
+for file in os.listdir(args.path):
+    if file.endswith(".pdf"):
+        d = loader.load_data(file=os.path.join(args.path, file))
+        if documents is None:
+            documents = d
+        else:
+            documents.append(d)
+
+
+
+query_str = QA_PROMPT_TMPL = (
+    "We have provided context information below. \n"
+    "---------------------\n"
+    "{context_str}"
+    "\n---------------------\n"
+    "Given this information, I want you to act as insurance assistance. I’ll write you my insurance info and my diagnosis , and you’ll inform for me which guaranteed amount, info from via my insurance terms and answer this : {query}\n"
 )
 
-# Load the your data
-documents = SimpleDirectoryReader('./data').load_data()
-index = ListIndex.from_documents(documents, service_context=service_context)
 
-# Query and print response
-query_engine = index.as_query_engine()
-response = query_engine.query("<query_text>")
+hf_predictor = HuggingFaceLLMPredictor(
+    max_input_size=2048,
+    max_new_tokens=256,
+    temperature=0.25,
+    do_sample=False,
+    query_wrapper_prompt=QA_PROMPT_TMPL,
+    tokenizer_name="LlamaTokenizer",
+    model_name=args.model,
+    device_map="auto",
+    tokenizer_kwargs={"max_length": 2048},
+    model_kwargs={"torch_dtype": torch.bfloat16},
+)
+
+embed_model = LangchainEmbedding(
+    HuggingFaceEmbeddings(model_name="sentence-transformers/ko-sroberta-multitask")
+)
+
+
+service_context = ServiceContext.from_defaults(
+    chunk_size_limit=512, llm_predictor=hf_predictor, embed_model=embed_model
+)
+
+
+QA_PROMPT = QuestionAnswerPrompt(QA_PROMPT_TMPL)
+index = VectorStoreIndex.from_documents(documents)
+
+index.storage_context.persist()
+
+engine = index.as_query_engine(text_qa_template=QA_PROMPT)
+response = engine.query(query_str)
+
 print(response)
